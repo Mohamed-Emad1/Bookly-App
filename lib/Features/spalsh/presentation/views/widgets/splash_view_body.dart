e4b3dd1d0837dadd06237f4cca4ed676b530016f
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-09-04 16:35:55 +0300

    refactor for init state and create home page

--- lib/Features/spalsh/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/spalsh/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,11 @@
+import 'package:booly_app/Features/home/presentation/views/home_view.dart';
 import 'package:booly_app/Features/spalsh/presentation/views/widgets/sliding_text.dart';
+import 'package:booly_app/constants.dart';
 import 'package:booly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class SpalshViewBody extends StatefulWidget {
   const SpalshViewBody({super.key});
@@ -16,19 +21,17 @@ class _SpalshViewBodyState extends State<SpalshViewBody>
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
+    initSlidindAnimation();
 
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
-            .animate(animationController);
+    navigateToHome();
+  }
 
-    animationController.forward();
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(const HomeView(), transition: Transition.fadeIn, duration: kTransition);
+    });
   }
 
   @override
@@ -48,5 +51,18 @@ class _SpalshViewBodyState extends State<SpalshViewBody>
         SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidindAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/services.dart';
+
 const kPrimaryColor = Color(0xFF100B20);
+const kTransition = Duration(seconds: 2);
