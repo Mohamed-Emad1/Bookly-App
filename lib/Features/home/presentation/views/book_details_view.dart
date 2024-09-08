2a5a63ccd475f3ef0a719699ee9a17dc9e59f944
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-09-08 18:59:34 +0300

    create book details view

---
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,11 @@
+import 'package:booly_app/Features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailView extends StatelessWidget {
+  const BookDetailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BookDetailsViewBody();
+  }
+}
