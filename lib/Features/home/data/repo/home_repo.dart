79a67bd5d29dd0edd5790985aa179fc48a7c6ebb
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-09-13 18:28:32 +0300

    create server error from dio

--- lib/Features/home/data/repo/home_repo.dart
+++ lib/Features/home/data/repo/home_repo.dart
@@ -2,7 +2,7 @@ import 'package:booly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:booly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo{
-  Future<Either< Failure ,List<BookModel>>> fetchBestSellerBooks();
-  Future<Either<Failure, List<BookModel>>>  fetchFeaturedBooks();
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/Features/home/data/repo/home_repo_implementaion.dart
+++ lib/Features/home/data/repo/home_repo_implementaion.dart
@@ -1,13 +1,34 @@
 import 'package:booly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:booly_app/Features/home/data/repo/home_repo.dart';
 import 'package:booly_app/core/errors/failure.dart';
+import 'package:booly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoImplementaion extends HomeRepo{
+class HomeRepoImplementaion extends HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImplementaion({required this.apiService});
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              "https://www.googleapis.com/books/v1/volumes?Filtering=free-ebooks&Sorting=newest &q=Programming");
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure(message: e.message!));
+      }
+
+      return left(ServerFailure(message: e.toString()));
+    }
   }
 
   @override
@@ -15,4 +36,4 @@ class HomeRepoImplementaion extends HomeRepo{
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
- }
+}
