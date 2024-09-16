a6012c8a551d2afc087ad8db1c1657bd69053396
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-09-16 11:36:33 +0300

    implement similar books

--- lib/Features/home/data/repo/home_repo_implementaion.dart
+++ lib/Features/home/data/repo/home_repo_implementaion.dart
@@ -52,4 +52,26 @@ class HomeRepoImplementaion extends HomeRepo {
       return left(ServerFailure(message: e.toString()));
     }
   }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async{
+           try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&Sorting=relevance&q=Programming");
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure(message: e.message!));
+      }
+
+      return left(ServerFailure(message: e.toString()));
+    }
+  }
 }
